dea24ee2ccafc08038acfd9c9d82fa3287a0a01c
daniel.benporat@post.runi.ac.il <gzb.dbp@gmail.com>
2025-10-29 21:12:38 +0200

    added bill3

--- Bill3.java
+++ Bill3.java
@@ -1,18 +1,21 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// reciving the input off the names
+		if (args.length != 4) {
+			System.out.println("Usage: java Bill3 <name1> <name2> <name3> <bill>");
+			return;
+		}
+
+		//reciving the input off the names
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
 		
 		//reciving the input off the bill dividing by 3 and round it up
-		double bill = Double.parseDouble(args[3]);
-		bill=Math.ceil(bill / 3.0);
-
+		double bill = Math.ceil(Double.parseDouble(args[3]) / 3.0);
+		
 		//printing the answer
-		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +
-							 ": pay " + bill + "Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +": pay " + bill + "Shekels each.");
 
 	}
 }
