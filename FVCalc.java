df85741538b28c27ef05a1a460a5523ce7b202d1
daniel.benporat@post.runi.ac.il <gzb.dbp@gmail.com>
2025-10-29 22:14:40 +0200

    debugging FVCalc 2

--- FVCalc.java
+++ FVCalc.java
@@ -3,15 +3,14 @@ public class FVCalc {
 	public static void main(String[] args){
 		// reciving inputs
 		int currentValue = Integer.parseInt(args[0]);
-		double rate = Double.parseDouble(args[1]) / 100;
+		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
 
 		//calculating future value
-		rate = rate + 1;
-		double futureValue = currentValue * Math.pow(rate, years);
+		int futureValue = (int)(currentValue * Math.pow(rate / 100 + 1, years));
 		
-
-		System.out.println("After " + years + " years, $" + currentValue + " saved at " + rate + ".0% will yield $" + futureValue);
+		//printing future value
+		System.out.println("After " + years + " years, $" + currentValue + " saved at " + rate + "% will yield $" + futureValue);
 
 	}
 }
