8dd85c19bc8f6bdd4a329d367886a2ff363f4740
Daniel BP <danielbp@Yaniv.local>
2025-10-29 15:41:32 +0200

    First Excices check

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,18 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+		// reciving the input off the names
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		
+		//reciving the input off the bill dividing by 3 and round it up
+		double bill = Double.parseDouble(args[3]);
+		bill=Math.ceil(bill / 3.0);
+
+		//printing the answer
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +
+		 ": pay " + bill + "Shekels each.");
+
 	}
 }
---
+++ PrintTo5.java
@@ -0,0 +1,19 @@
+// Prints the numbers 0 to 5.
+// (Same program as shown in lecture 1-1).
+public class PrintTo5 {
+	public static void main(String[] args) {
+		// Declares an int variable, and sets it to 0
+        int i = 0;
+
+        // Complete the program's code here:
+
+        // prints numbers 0-5
+        while (i <6){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Done");
+        
+
+    }
+}
